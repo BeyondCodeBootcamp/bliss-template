62a2afa7c1248a2276ef45d1511a9536ed30ee9e
AJ ONeal <coolaj86@gmail.com>
2021-08-04 11:49:46 -0600

    feature: auto-update theme

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,20 @@
 set -e
 set -u
 
+# Auto-update script
 curl https://raw.githubusercontent.com/BeyondCodeBootcamp/bliss-template/upgrade/scripts/ga-update.sh > scripts/ga-update.sh
 bash ./scripts/ga-update.sh
 
+# Update submodules
 git submodule init
 git submodule update
 
+# Update theme
+rm -rf themes/eon
+# Get the latest v0 with bugfixes
+git clone --depth 1 --branch v0 https://github.com/coolaj86/eon themes/eon
+
+# Build
 rm -rf ./bliss.tpl/
 rm -rf ./public/
 hugo
@@ -19,6 +27,7 @@ npm ci
 rm -rf ./themes/eon/node_modules
 mv ./node_modules ./themes/eon/node_modules
 
+# Dist
 ./themes/eon/scripts/dist-prettier
 ./themes/eon/scripts/dist-remove-empty-lines
 # Depends on https://github.com/ryanburnette/eon/pull/8
