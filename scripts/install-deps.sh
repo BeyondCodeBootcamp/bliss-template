78f716c0ab0d4202eb89f615ecdd8846b281cf6a
AJ ONeal <coolaj86@gmail.com>
2022-06-01 10:47:51 -0600

    build: upgrade to hugo v0.100

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,5 +2,5 @@
 
 curl -sS https://webinstall.dev/sd@v0.7 | bash
 curl -sS https://webinstall.dev/jq@v1.6 | bash
-curl -sS https://webinstall.dev/hugo@v0.86 | bash
+curl -sS https://webinstall.dev/hugo@v0.100 | bash
 curl -sS https://webinstall.dev/node@v16 | bash
