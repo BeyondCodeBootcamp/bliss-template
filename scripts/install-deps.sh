090ee4f565d293a027422c754cf1ad4ee4532f35
AJ ONeal <coolaj86@proton.me>
2024-08-07 16:21:28 -0600

    chore: update deps

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-curl -sS https://webinstall.dev/sd@v0.7 | bash
-curl -sS https://webinstall.dev/jq@v1.6 | bash
-curl -sS https://webinstall.dev/hugo@v0.100 | bash
-curl -sS https://webinstall.dev/node@v16 | bash
+curl -sS https://webinstall.dev/sd@v0.7 | sh
+curl -sS https://webinstall.dev/jq@v1.6 | sh
+curl -sS https://webinstall.dev/hugo@v0.131 | sh
+curl -sS https://webinstall.dev/node@v22 | sh
